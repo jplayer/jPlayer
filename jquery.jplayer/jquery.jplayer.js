68658eb93e02a83f6e5d9489eff86b561d8b40cb
Dennis S. Hennen <dennis@28studios.com>
2011-03-01 10:59:04 -0500

    Do not try to unbind on non-existent elements.

--- jquery.jplayer/jquery.jplayer.js
+++ jquery.jplayer/jquery.jplayer.js
@@ -634,7 +634,9 @@
 				this.pause(); // Pauses the media and clears any delayed commands used in the HTML solution.
 			}
 			$.each(this.css.jq, function(fn, jq) { // Remove any bindings from the interface controls.
-				jq.unbind(".jPlayer");
+				if (jq.unbind) {
+					jq.unbind(".jPlayer");
+				}
 			});
 			this.element.removeData("jPlayer"); // Remove jPlayer data
 			this.element.unbind(".jPlayer"); // Remove all event handlers created by the jPlayer constructor
