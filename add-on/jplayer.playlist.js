05266019b6310544eee45b86133a3d1c19920d28
Mark Panaghiston <markp@happyworm.com>
2014-06-09 18:26:21 +0100

    Playlist - Fixed free DL link's tabindex to -1 to remove it from order.

--- add-on/jplayer.playlist.js
+++ add-on/jplayer.playlist.js
@@ -261,7 +261,7 @@
 						} else {
 							listItem += " | ";
 						}
-						listItem += "<a class='" + self.options.playlistOptions.freeItemClass + "' href='" + value + "'>" + property + "</a>";
+						listItem += "<a class='" + self.options.playlistOptions.freeItemClass + "' href='" + value + "' tabindex='-1'>" + property + "</a>";
 					}
 				});
 				listItem += ")</span>";
