e83d1dd92ceba41215fce41583d32a535a477a40
Mark Panaghiston <markp@happyworm.com>
2014-09-01 19:13:59 +0100

    Updated popcorn player plugin versioning and mp3 codec fix.

--- popcorn/player/popcorn.jplayer.js
+++ popcorn/player/popcorn.jplayer.js
@@ -7,11 +7,11 @@
  * http://opensource.org/licenses/MIT
  *
  * Author: Mark J Panaghiston
- * Version: 1.1.3
- * Date: 2nd April 2014
+ * Version: 1.1.4
+ * Date: 1st September 2014
  *
  * For Popcorn Version: 1.3
- * For jPlayer Version: 2.6.0
+ * For jPlayer Version: 2.7.0
  * Requires: jQuery 1.7+
  * Note: jQuery dependancy cannot be removed since jPlayer 2 is a jQuery plugin. Use of jQuery will be kept to a minimum.
  */
@@ -22,16 +22,16 @@
 
 (function(Popcorn) {
 
-	var JQUERY_SCRIPT = '//code.jquery.com/jquery-1.11.0.min.js', // Used if jQuery not already present.
-	JPLAYER_SCRIPT = '//www.jplayer.org/2.6.0/js/jquery.jplayer.min.js', // Used if jPlayer not already present.
-	JPLAYER_SWFPATH = '//www.jplayer.org/2.6.0/js/Jplayer.swf', // Used if not specified in jPlayer options via SRC Object.
+	var JQUERY_SCRIPT = '//code.jquery.com/jquery-1.11.1.min.js', // Used if jQuery not already present.
+	JPLAYER_SCRIPT = '//www.jplayer.org/2.7.0/js/jquery.jplayer.min.js', // Used if jPlayer not already present.
+	JPLAYER_SWFPATH = '//www.jplayer.org/2.7.0/js/Jplayer.swf', // Used if not specified in jPlayer options via SRC Object.
 	SOLUTION = 'html,flash', // The default solution option.
 	DEBUG = false, // Decided to leave the debugging option and console output in for the time being. Overhead is trivial.
 	jQueryDownloading = false, // Flag to stop multiple instances from each pulling in jQuery, thus corrupting it.
 	jPlayerDownloading = false, // Flag to stop multiple instances from each pulling in jPlayer, thus corrupting it.
 	format = { // Duplicate of jPlayer 2.5.0 object, to avoid always requiring jQuery and jPlayer to be loaded before performing the _canPlayType() test.
 		mp3: {
-			codec: 'audio/mpeg; codecs="mp3"',
+			codec: 'audio/mpeg;',
 			flashCanPlay: true,
 			media: 'audio'
 		},
